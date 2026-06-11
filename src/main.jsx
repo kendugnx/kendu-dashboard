a6aa3af7dc2e9bb364795fad9b58c354654994fc
kendugnx <kendugnx@gmail.com>
2026-06-11 10:09:00 -0400

    Add Telegram Web App integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (window.Telegram?.WebApp) {
+  window.Telegram.WebApp.expand()
+  window.Telegram.WebApp.ready()
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
